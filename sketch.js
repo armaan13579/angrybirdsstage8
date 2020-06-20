1170e63cf5ee1f20bd06e339ca2d60049b465bbc
armaan <t.yasmintaj2@gmail.com>
2020-06-20 19:50:51 +0530

    api calls

--- sketch.js
+++ sketch.js
@@ -11,14 +11,14 @@ var bird, slingshot;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getbackgroundimg();
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    getbackgroundimg();
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -45,6 +45,7 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)   
     background(backgroundImg);
     Engine.update(engine);
     //strokeWeight(4);
@@ -85,4 +86,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getbackgroundimg(){
+    var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responsej = await response.json();
+    var datetime = responsej.datetime;
+    //console.log(datetime);
+    var hour = datetime.slice(11,13)
+    console.log(hour);
+    
+    if (hour>=06&&hour<=18)  {
+bg="sprites/bg.png";
+    }else{
+bg= "sprites/bg2.jpg";
+    }
+    backgroundImg=loadImage(bg);
 }
